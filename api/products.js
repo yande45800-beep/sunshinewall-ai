508a94710499aa4f14328fb0da65183edd5fcfb2
yande45800-beep <yande45800@gmail.com>
2025-10-27 18:54:32 +0100

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,21 +1,30 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   const limit = req.query?.limit || 20;
   const endpoint = "https://sunshinewallkl.myshopify.com/api/2023-10/graphql.json";
   const token = "24efc55d625f21341bc5dd932d056779";
 
-  const gqlQuery = `{
-    products(first: ${limit}) {
-      edges {
-        node {
-          id
-          title
-          handle
-          descriptionHtml
-          featuredImage { url altText }
+  const gqlQuery = `
+    {
+      products(first: ${limit}) {
+        edges {
+          node {
+            id
+            title
+            handle
+            descriptionHtml
+            featuredImage { url altText }
+            variants(first: 5) {
+              edges {
+                node {
+                  priceV2 { amount currencyCode }
+                }
+              }
+            }
+          }
         }
       }
     }
-  }`;
+  `;
 
   try {
     const response = await fetch(endpoint, {
@@ -30,7 +39,6 @@ module.exports = async (req, res) => {
     const data = await response.json();
     res.status(200).json(data);
   } catch (err) {
-    console.error("Shopify API error:", err);
     res.status(500).json({ error: err.message });
   }
-};
+}
